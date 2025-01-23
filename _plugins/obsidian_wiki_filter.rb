98de98e808c4f4ae87b3e9989dcc1d251826a00a
Wichai515 <wichai_pan@outlook.com>
2025-01-23 13:24:24 +0000

    test

--- _plugins/obsidian_wiki_filter.rb
+++ _plugins/obsidian_wiki_filter.rb
@@ -3,9 +3,9 @@ Jekyll::Hooks.register :documents, :pre_render do |doc|
       link_text = $1
       if link_text.include?('|')
         parts = link_text.split('|')
-        "[#{parts[1]}](#{parts[0]}.html)"
+        "[#{parts[1]}](#{parts[0]}.md)"
       else
-        "[#{link_text}](#{link_text}.html)"
+        "[#{link_text}](#{link_text}.md)"
       end
     end
   end
